1741fc9583836bb1878f746c0cfd174f866bc576
pranav99d <pranavdere99@gmail.com>
2022-03-24 14:35:00 +0530

    Initial Commit

---
+++ autoWiringByName/src/org/com/Capital.java
@@ -0,0 +1,13 @@
+package org.com;
+
+public class Capital {
+	String capitalName;
+
+	public String getCapitalName() {
+		return capitalName;
+	}
+
+	public void setCapitalName(String capitalName) {
+		this.capitalName = capitalName;
+	}
+}
