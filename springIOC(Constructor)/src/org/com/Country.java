18c49481d33e4137e5f3a2ed862f6d76c3b8c401
pranav99d <pranavdere99@gmail.com>
2022-03-24 14:32:33 +0530

    Initial Commit

---
+++ springIOC(Constructor)/src/org/com/Country.java
@@ -0,0 +1,18 @@
+package org.com;
+
+public class Country {
+    String countryName;
+   Capital capital;
+ 
+   public Country(String countryName, Capital capital) {
+       super();
+       this.countryName = countryName;
+       this.capital = capital;
+   }
+   public String getCountryName() {
+       return countryName;
+   }
+      public Capital getCapital() {
+       return capital;
+   }
+ }
