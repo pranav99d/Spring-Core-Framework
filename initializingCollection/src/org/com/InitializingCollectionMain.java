c8bab87f9c9d58990bdf8a8ea2bb4bbff43e6d39
pranav99d <pranavdere99@gmail.com>
2022-03-24 14:33:32 +0530

    Initial Commit

---
+++ initializingCollection/src/org/com/InitializingCollectionMain.java
@@ -0,0 +1,11 @@
+package org.com;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ public class InitializingCollectionMain{
+     public static void main(String[] args) {
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        Country countryObj = (Country) appContext.getBean("CountryBean");
+        countryObj.printListOfStates();
+    }
+}
