355e397daef8e5d0b52a2462acc23b128e4d7b8e
pranav99d <pranavdere99@gmail.com>
2022-03-24 16:08:33 +0530

    Initial Commit

---
+++ springMavenProject/src/main/java/com/springMaven/Country.java
@@ -0,0 +1,19 @@
+package com.springMaven;
+
+public class Country {
+
+	String countryName;
+
+	public Country(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+}
