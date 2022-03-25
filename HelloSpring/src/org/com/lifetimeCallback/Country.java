42b328b734c6551745abcd053cb87af0aaa9c9cd
pranav99d <pranavdere99@gmail.com>
2022-03-25 08:38:16 +0530

    Spring Lifetime Callbacks

---
+++ HelloSpring/src/org/com/lifetimeCallback/Country.java
@@ -0,0 +1,21 @@
+package org.com.lifetimeCallback;
+
+public class Country {
+	String countryName;
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public void init() {
+		System.out.println("In init block of country");
+	}
+
+	public void destroy() {
+		System.out.println("In destroy block of country");
+	}
+}
