c995202583c9a2efef9896f6bcdcd3740683351d
pranav99d <pranavdere99@gmail.com>
2022-03-27 15:09:57 +0530

    Initial Commit

---
+++ springBeanPostProcessors/src/org/com/BeanPostProcessorExampleMain.java
@@ -0,0 +1,14 @@
+package org.com;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanPostProcessorExampleMain {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		AbstractApplicationContext appContext = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		Country countryObj = (Country) appContext.getBean("country");
+		System.out.println("Country Name: " + countryObj.getCountryName());
+		appContext.registerShutdownHook();
+	}
+}
